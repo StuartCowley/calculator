ddc103f491d0dfc2e462a9184b7b02bc5f5c5534
StuartCowley <voidstalker@live.co.uk>
2019-08-11 21:13:08 +0100

    worked on addition logic and egenral react revision

--- src/components/CalcFullDisplay.js
+++ src/components/CalcFullDisplay.js
@@ -3,93 +3,173 @@ import CalcScreen from "./CalcScreen";
 
 class CalcFullDisplay extends React.Component {
   state = {
-    display: 0,
-    total: 0,
+    display: 0,   
+    total: 0,     
     temp: 0,
-    numString: 0
+    firstInput: 0,
+    runningTotal: 0  
   };
 
-// ISSUES:
-// Updating the state from other props. Can this be done? Trying to find a way to update
-// the screen total based on the running subtotal ass it changes
-
-  getInput = (key) => {
-    const currentNum = this.state.numString;
-    this.setState({ numString: currentNum + key })
-    this.setState({ display: this.state.numString })
-  }
-  addButton = () => {
-    const currentNum = Number(this.state.numString);
-    console.log(currentNum)
-    if (this.state.temp === 0) {
-      this.setState({ total: currentNum })
-      this.setState({ numString: 0 })
-    } else {
-      this.setState({ temp: currentNum });
-      this.setState({ numString: 0 })
+  getInput = key => {
+    if (this.state.runningTotal === this.state.display) {
+      this.setState({
+        display: 0
+      })
     }
-    console.log(this.state.total)
-  };
+      let currentDisplay = Number(this.state.display);
+      let text = Number(currentDisplay + key)
+      this.setState({
+        temp: text,
+        display: text
+      })
+    };
+
+  addButton = () => {
+    let temp = this.state.temp;
+    let firstInput = Number(this.state.firstInput);
+    let runningTotal = Number(this.state.runningTotal);
+    let display = Number(this.state.display);
+
+    if (firstInput===0) {
+      this.setState({
+        firstInput: temp,
+        runningTotal: runningTotal + temp,
+        temp: 0,
+        display: 0
+      })
+    } else if (firstInput!==0) {
+      let newTotal = Number(firstInput + display);
+      this.setState({
+        temp: 0,
+        runningTotal: newTotal,
+        display: newTotal
+      })
+    } 
+    
+    // console.log('temp: ' + this.state.temp)
+    // console.log('first: ' + this.state.firstInput)
+    // console.log('running: ' + this.state.runningTotal)
+    }
 
   subButton = () => {
-    const current = this.state.total;
-    this.setState({ total: current - 1 });
+    let current = this.state.total;
+    this.setState({
+      total: current - 1
+    });
   };
   multiplyButton = () => {
-    const current = this.state.total;
-    this.setState({ total: current * 2 });
+    let current = this.state.total;
+    this.setState({
+      total: current * 2
+    });
   };
   divideButton = () => {
-    const current = this.state.total;
-    this.setState({ total: current / 2 });
+    let current = this.state.total;
+    this.setState({
+      total: current / 2
+    });
   };
   equalsButton = () => {
-    const runningTotal = this.state.total;
-    const current = Number(this.state.numString);
-    this.setState({ total: runningTotal + current })
+    // let currentNum = Number(this.state.temp);
+    // let val = Number(this.state.total);
+    // let newTotal = Number(currentNum + val);
 
+
+    let runningTotal = this.state.total;
+    let current = Number(this.state.runningTotal);
+    this.setState({
+      total: runningTotal + current
+    });
   };
+
+  clearButton = () => {
+    this.setState({
+      display: 0
+    })
+  };
+
   clearAllButton = () => {
-    this.setState({ display: 0 });
-    this.setState({ total: 0 });
-    this.setState({ temp: 0 });
-    this.setState({ numString: '0' });
+    this.setState({
+      display: 0,
+      total: 0,
+      firstInput: 0,
+      runningTotal: 0,
+      temp: 0
+    });
   }
+
 
   render() {
     return (
       <div id="calcContainer">
-        <CalcScreen temp={Number(this.state.total)} />
+        <CalcScreen temp={Number(this.state.display)} />
         <div>
-          <button onClick={() => this.clearAllButton()} className="clearButton">Clear</button>
-          <button onClick={() => this.clearAllButton()} className="clearButton">All Clear</button>
+          <button onClick={() => this.clearButton()} className="clearButton">
+            Clear
+          </button>
+          <button onClick={() => this.clearAllButton()} className="clearButton">
+            All Clear
+          </button>
         </div>
         <div>
-          <button onClick={() => this.getInput('7')} className="smallButton">7</button>
-          <button onClick={() => this.getInput('8')} className="smallButton">8</button>
-          <button onClick={() => this.getInput('9')} className="smallButton">9</button>
-          <button onClick={this.divideButton} className="smallButton">%</button>
+          <button onClick={() => this.getInput("7")} className="smallButton">
+            7
+          </button>
+          <button onClick={() => this.getInput("8")} className="smallButton">
+            8
+          </button>
+          <button onClick={() => this.getInput("9")} className="smallButton">
+            9
+          </button>
+          <button onClick={this.divideButton} className="smallButton">
+            %
+          </button>
         </div>
         <div>
-          <button onClick={() => this.getInput('4')} className="smallButton">4</button>
-          <button onClick={() => this.getInput('5')} className="smallButton">5</button>
-          <button onClick={() => this.getInput('6')} className="smallButton">6</button>
-          <button onClick={this.multiplyButton} className="smallButton">x</button>
+          <button onClick={() => this.getInput("4")} className="smallButton">
+            4
+          </button>
+          <button onClick={() => this.getInput("5")} className="smallButton">
+            5
+          </button>
+          <button onClick={() => this.getInput("6")} className="smallButton">
+            6
+          </button>
+          <button onClick={this.multiplyButton} className="smallButton">
+            x
+          </button>
         </div>
         <div>
-          <button onClick={() => this.getInput('1')} className="smallButton">1</button>
-          <button onClick={() => this.getInput('2')} className="smallButton">2</button>
-          <button onClick={() => this.getInput('3')} className="smallButton">3</button>
-          <button onClick={this.addButton} className="smallButton">+</button>
+          <button onClick={() => this.getInput("1")} className="smallButton">
+            1
+          </button>
+          <button onClick={() => this.getInput("2")} className="smallButton">
+            
+            2
+          </button>
+          <button onClick={() => this.getInput("3")} className="smallButton">
+            
+            3
+          </button>
+          <button onClick={this.addButton} className="smallButton">
+            +
+          </button>
         </div>
         <div>
-          <button onClick={() => this.getInput('0')} className="doubleButton">0</button>
-          <button onClick={() => this.equalsButton()} className="smallButton">=</button>
-          <button onClick={this.subButton} className="smallButton">-</button>
+          <button onClick={() => this.getInput("0")} className="doubleButton">
+            
+            0
+          </button>
+          <button onClick={() => this.equalsButton()} className="smallButton">
+            =
+          </button>
+          <button onClick={this.subButton} className="smallButton">
+            -
+          </button>
         </div>
-      </div>  
+      </div>
     );
   }
-};
+}
 
 export default CalcFullDisplay;
