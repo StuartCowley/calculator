a4fe7d95957ff47560177bcf4e447d4737495821
StuartCowley <voidstalker@live.co.uk>
2019-08-14 17:09:51 +0100

    Updates

---
+++ precache-manifest.02145476509ed9638468e4d231370cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82f2237b05f0c7829a4814483b49ae7",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "79d1073600a90685d576",
+    "url": "/calculator/static/css/main.c2a845c9.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/calculator/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "79d1073600a90685d576",
+    "url": "/calculator/static/js/main.37302ca9.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "f5be4a6806c1c385c1222aeae4fe66a2",
+    "url": "/calculator/static/media/blackboard.f5be4a68.jpg"
+  }
+]);
